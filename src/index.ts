dcc96793219a4dec3ea59a7e8ac3d0fe57a3c87a
Bartosz Jakubowiak <bartosz@jakubowiak.pl>
2024-03-03 01:42:49 +0100

    clean up, rename the package

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,10 @@
 import cluster from "cluster";
-import process from "process";
 
 import { primaryThread } from "./treads/primary.js";
 import { workerThread } from "./treads/wokrerThread.js";
 
-const run = async () => {
-  
-  if (cluster.isPrimary) {
-    await primaryThread();
-    process.exit(0);
-  } else {
-    workerThread();
-  }
-};
-
-run();
+if (cluster.isPrimary) {
+  primaryThread();
+} else {
+  workerThread();
+}
--- src/treads/primary.ts
+++ src/treads/primary.ts
@@ -8,86 +8,89 @@ import cliProgress from "cli-progress";
 import { SeedData } from "../types/config.js";
 import config from "../config.js";
 
-
 const numberOfCPUs = os.cpus().length;
 
 export const primaryThread = async () => {
+  const {
+    raw,
+    translated,
+    devMode,
+    stepResolution,
+    timerStart,
+    offsetInSeconds,
+    timerEnd,
+    startTime,
+  } = await prepareDataset();
 
-    return new Promise<void>(async (resolve) => {
+  let fileName;
+  if (devMode) {
+    fileName = "chart";
+  } else {
+    fileName = `chart - ${new Date().toISOString()}`;
+  }
 
-    
-    
-    const { raw, translated, devMode, stepResolution, timerStart, offsetInSeconds, timerEnd, startTime } = await prepareDataset();
+  const timerStartFromMidinght = getSecondsFromHourString(
+    timerStart,
+    offsetInSeconds
+  );
+  const timerStopFromMidinght = getSecondsFromHourString(
+    timerEnd,
+    offsetInSeconds
+  );
+  const videoRecordingStart = getSecondsFromHourString(
+    startTime,
+    offsetInSeconds
+  );
 
-    let fileName;
-    if (devMode) {
-      fileName = "chart";
-    } else {
-      fileName = `chart - ${new Date().toISOString()}`;
-    }
+  const timerStartSecond =
+    (timerStartFromMidinght - videoRecordingStart) / 1000;
+  const timerRunInSeconds =
+    (timerStopFromMidinght - timerStartFromMidinght) / 1000;
+  const timerStoptSecond = timerStartSecond + timerRunInSeconds;
 
-    const timerStartFromMidinght = getSecondsFromHourString(
-      timerStart,
-      offsetInSeconds
+  jsonArray(raw, fileName);
+
+  if (config.textOnly) {
+    console.log(
+      "Only data.txt rendered. Turn off textOnly to render the full chart!"
     );
-    const timerStopFromMidinght = getSecondsFromHourString(
-      timerEnd,
-      offsetInSeconds
-    );
-    const videoRecordingStart = getSecondsFromHourString(
-      startTime,
-      offsetInSeconds
-    );
+    return;
+  }
 
-    const timerStartSecond =
-      (timerStartFromMidinght - videoRecordingStart) / 1000;
-    const timerRunInSeconds =
-      (timerStopFromMidinght - timerStartFromMidinght) / 1000;
-    const timerStoptSecond = timerStartSecond + timerRunInSeconds;
+  try {
+    fs.mkdirSync(`./out/${fileName}`);
+  } catch {}
 
-    jsonArray(raw, fileName);
+  let res: number = 0;
 
-    if (config.textOnly) {
-      console.log("Only data.txt rendered. Turn off textOnly to render the full chart!")
-      return;
-    }
+  const bar1 = new cliProgress.SingleBar(
+    {
+      etaBuffer: 1000,
+      etaAsynchronousUpdate: true,
+    },
+    cliProgress.Presets.shades_classic
+  );
+  bar1.start(translated.length, 0);
 
-    try {
-      fs.mkdirSync(`./out/${fileName}`);
-    } catch {
+  for (let i = 0; i < numberOfCPUs; i++) {
+    let worker = cluster.fork({
+      chunk: i,
+      chunks: numberOfCPUs,
+      fileName,
+      translated: JSON.stringify(translated),
+      timerStartSecond,
+      timerStoptSecond,
+      stepResolution,
+      devMode,
+    } as SeedData);
 
-    }
-
-    let res: number = 0;
-
-    const bar1 = new cliProgress.SingleBar(
-      {},
-      cliProgress.Presets.shades_classic
-    );
-    bar1.start(translated.length, 0);
-
-
-
-    for (let i = 0; i < numberOfCPUs; i++) {
-      let worker = cluster.fork({
-        chunk: i,
-        chunks: numberOfCPUs,
-        fileName,
-        translated: JSON.stringify(translated),
-        timerStartSecond,
-        timerStoptSecond,
-        stepResolution,
-        devMode
-      } as SeedData);
-
-      worker.on("message", () => {
-        bar1.increment();
-        res++;
-        if (res == translated.length) {
-          bar1.stop();
-          resolve();
-        }
-      });
-    }
-})
-}
+    worker.on("message", () => {
+      bar1.increment();
+      res++;
+      if (res == translated.length) {
+        bar1.stop();
+        process.exit();
+      }
+    });
+  }
+};
