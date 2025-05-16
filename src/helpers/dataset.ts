2b256728e3f2f0065a3a7a975aa60750d913b80c
Bartosz Jakubowiak <bartosz@jakubowiak.pl>
2025-05-16 17:47:34 +0200

    cleanup types in dataset.ts

--- src/helpers/dataset.ts
+++ src/helpers/dataset.ts
@@ -1,46 +1,52 @@
 import config from "../config.js";
-import fs from "fs";
 // import csv from "csvtojson";
-import { PolarMeta, PolarSession } from "../types/Polar.js";
 import { getSecondsFromHourString } from "./time.js";
-import { DataSection, DataSource, NormalisedDataSection } from "../types/config.js";
+import { DataSource, NormalisedDataSection } from "../types/config.js";
 import { KeyedDataset } from "../types/dataparsers.js";
 
-
-
-
-export const prepareDataset = async (normalisedDataSets: { [k in DataSource]?: KeyedDataset }): Promise<NormalisedDataSection[]> => {
-
+export const prepareDataset = async (normalisedDataSets: {
+  [k in DataSource]?: KeyedDataset;
+}): Promise<NormalisedDataSection[]> => {
   const normalisedDataSections: NormalisedDataSection[] = [];
 
-  config.sections.forEach(section => {
+  config.sections.forEach((section) => {
+    const startTime = getSecondsFromHourString(
+      section.startTime,
+      config.secondsAligment
+    );
+    const endTime = getSecondsFromHourString(
+      section.endTime,
+      config.secondsAligment
+    );
 
-    const startTime = getSecondsFromHourString(section.startTime, config.secondsAligment);
-    const endTime = getSecondsFromHourString(section.endTime, config.secondsAligment);
-
-
-    const timerStart = getSecondsFromHourString(section.timerStart, config.secondsAligment);
-    const timerEnd = getSecondsFromHourString(section.timerEnd, config.secondsAligment);
+    const timerStart = getSecondsFromHourString(
+      section.timerStart,
+      config.secondsAligment
+    );
+    const timerEnd = getSecondsFromHourString(
+      section.timerEnd,
+      config.secondsAligment
+    );
 
     const timerStartIndex = timerStart - startTime;
     const timerSeconds = timerEnd - timerStart;
     const graphSeconds = endTime - startTime;
 
-    const slicedOutput: any = {}
+    const slicedOutput: any = {};
     for (const k in section.use) {
-
       if (!normalisedDataSets[k as DataSource]) {
         throw Error(`${k} is not defined in sources`);
       }
 
       slicedOutput[k] = {
         label: section.use[k as DataSource]?.label,
-        dataPoints: []
-      }
+        dataPoints: [],
+      };
       for (let i = startTime; i <= startTime + graphSeconds; i++) {
-        slicedOutput[k].dataPoints.push(normalisedDataSets[k as DataSource]![i]);
+        slicedOutput[k].dataPoints.push(
+          normalisedDataSets[k as DataSource]![i]
+        );
       }
-    
     }
 
     normalisedDataSections.push({
@@ -49,12 +55,9 @@ export const prepareDataset = async (normalisedDataSets: { [k in DataSource]?: K
       timerSeconds,
       prependAudioSeconds: section.prependAudioSeconds,
       appendAudioSeconds: section.appendAudioSeconds,
-      use: slicedOutput
-    })
-
-  })
-
+      use: slicedOutput,
+    });
+  });
 
   return normalisedDataSections;
-
 };
